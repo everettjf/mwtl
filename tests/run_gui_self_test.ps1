1b0097eb4181d02e8bcab376944313f267ddc38c
Everett <everettjf@live.com>
2026-08-07 20:33:04 -0700

    Report GUI self-test launch errors

---
+++ tests/run_gui_self_test.ps1
@@ -0,0 +1,27 @@
+param(
+    [Parameter(Mandatory = $true)] [string] $Executable,
+    [Parameter(Mandatory = $true)] [string] $RuntimePath
+)
+
+$ErrorActionPreference = 'Stop'
+$environmentPath = $env:PATH
+$info = [System.Diagnostics.ProcessStartInfo]::new()
+$info.FileName = $Executable
+$info.Arguments = '--self-test'
+$info.UseShellExecute = $false
+$info.EnvironmentVariables['PATH'] = "$RuntimePath;$environmentPath"
+$info.EnvironmentVariables['MWTL_TEST_NO_DIALOGS'] = '1'
+
+try {
+    $process = [System.Diagnostics.Process]::Start($info)
+} catch [System.ComponentModel.Win32Exception] {
+    Write-Error ('Could not start {0} (Win32 {1}): {2}' -f
+        $Executable, $_.Exception.NativeErrorCode, $_.Exception.Message)
+    exit 1
+}
+
+$process.WaitForExit()
+if ($process.ExitCode -ne 0) {
+    Write-Error "$Executable exited with code $($process.ExitCode)."
+}
+exit $process.ExitCode
